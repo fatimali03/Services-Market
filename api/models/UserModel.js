aee55017eabbbb30a3e9d1bba55c716798257693
Fatima Ali- Paysys <136559675+fatimali03@users.noreply.github.com>
2025-03-20 15:24:37 +0500

    Add files via upload

---
+++ api/models/UserModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const UserModel = new Schema(
+  {
+    fullName: String,
+    age: Number,
+    email: { type: String, unique: true },
+    username: { type: String, unique: true },
+    password: String,
+    company: {type: String, required: false},
+    role: String,
+    
+    image: { type: String, default: null },
+
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("users", UserModel);
